8c2f6076a48bafd09bfcfd0bbc40ade30253cd35
Charletta <charlettabullard12@outlook.com>
2018-11-08 08:55:34 -0500

    Installed jwt dependency and implemented it in server.js

--- client/server.js
+++ client/server.js
@@ -1,12 +1,31 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const port = 5000;
 
 const server = express();
 server.use(express.json());
 
+//JWT data
+server.generateJwt = function() {
+	var expiry = new Date();
+	expiry.setDate(expiry.getDate() + 7);
+
+	return jwt.sign(
+		{
+			_id: this._id,
+			email: this.email,
+			name: this.name,
+			exp: parseInt(expiry.getTime() / 1000),
+		},
+		'MY_SECRET',
+	); // DO NOT KEEP YOUR SECRET IN THE CODE!
+};
+
 //Test route
 server.get('/api', (req, res) => {
 	res.send('Working');
 });
+
+//Sign Up routes
 
 server.listen(port, () => console.log(`server running on port 5000`));
