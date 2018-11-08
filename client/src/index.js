d12aa7c5802e37d57a4b9c3b74a7845829cf07d0
Charletta <charlettabullard12@outlook.com>
2018-11-08 18:41:26 -0500

    added reactrap.min.css to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
