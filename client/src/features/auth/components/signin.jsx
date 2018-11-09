09089f89f014539ae96fafc6d7e4b55db8903400
Charletta <charlettabullard12@outlook.com>
2018-11-08 19:15:50 -0500

    Created barebone forms using reactstrap

--- client/src/features/auth/components/signin.jsx
+++ client/src/features/auth/components/signin.jsx
@@ -1,8 +1,50 @@
 import React, { Component } from 'react';
+import { Col, Row, Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 class Signin extends Component {
 	render() {
-		return <div>This is a Sign-In Page</div>;
+		return (
+			<Form>
+				<Row form>
+					<Col md={6}>
+						<FormGroup>
+							<Label for="exampleEmail">Email</Label>
+							<Input
+								type="email"
+								name="email"
+								id="exampleEmail"
+								placeholder="with a placeholder"
+							/>
+						</FormGroup>
+					</Col>
+					<Col md={6}>
+						<FormGroup>
+							<Label for="examplePassword">Password</Label>
+							<Input
+								type="password"
+								name="password"
+								id="examplePassword"
+								placeholder="password placeholder"
+							/>
+						</FormGroup>
+					</Col>
+				</Row>
+				<FormGroup check>
+					<Input type="checkbox" name="check" id="exampleCheck" />
+					<Label for="exampleCheck" check>
+						Is Seeker
+					</Label>
+				</FormGroup>
+				<FormGroup check>
+					<Input type="checkbox" name="check" id="exampleCheck" />
+					<Label for="exampleCheck" check>
+						Is Employer
+					</Label>
+				</FormGroup>
+				<Button>Sign in</Button>
+			</Form>
+		);
 	}
 }
+
 export default Signin;
