48d437d824e335cc52ed3c42f48e5f720d6c5e8f
Charletta <charlettabullard12@outlook.com>
2018-11-08 23:43:46 -0500

    Signup page submits onclick with message in console but data still remains in the form, added TODO

--- client/src/features/auth/components/signup.jsx
+++ client/src/features/auth/components/signup.jsx
@@ -7,9 +7,53 @@ import {
 	FormGroup,
 	Label,
 	Input,
+	FormFeedback,
 } from 'reactstrap';
 
 class Signup extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			firstName: '',
+			lastName: '',
+			email: '',
+			password: '',
+			confirmPassword: '',
+			validate: {
+				emailState: '',
+			},
+		};
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	//validate event for email
+	validateEmail(e) {
+		const emailRex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		const { validate } = this.state;
+		if (emailRex.test(e.target.value)) {
+			validate.emailState = 'has-success';
+		} else {
+			validate.emailState = 'try-again';
+		}
+		this.setState({ validate });
+	}
+
+	// handle async and await changes to the email
+	handleChange = async event => {
+		const { target } = event;
+		const value = target.type === 'checkbox' ? target.checked : target.value;
+		const { name } = target;
+		await this.setState({ [name]: value });
+	};
+
+	// click event for sign-up button
+	//TODO: Find a way to get page to refresh or reload after submission
+	clickHandler(e) {
+		e.preventDefault();
+		// window.location.reload();
+		console.log('YOU HAVE SUCCESSFULLY SIGN-UP');
+	}
+
 	render() {
 		return (
 			<Container className='Signup'>
@@ -41,7 +85,17 @@ class Signup extends Component {
 								name='email'
 								id='exampleEmail'
 								placeholder='myemail@email.com'
+								value={this.state.email}
+								valid={this.state.validate.emailState === 'has-success'}
+								invalid={this.state.validate.emailState === 'try-again'}
+								onChange={e => {
+									this.validateEmail(e);
+									this.handleChange(e);
+								}}
 							/>
+							{/** FeedbackMsg for Valid Email and Invalid Email */}
+							<FormFeedback valid>Successful Email</FormFeedback>
+							<FormFeedback>Please use correct email format!</FormFeedback>
 						</FormGroup>
 					</Col>
 					<Col>
@@ -52,6 +106,10 @@ class Signup extends Component {
 								name='password'
 								id='examplePassword'
 								placeholder='********'
+								value={this.password}
+								onChange={e => {
+									this.handleChange(e);
+								}}
 							/>
 						</FormGroup>
 						<FormGroup>
@@ -61,7 +119,14 @@ class Signup extends Component {
 								name='password'
 								id='examplePassword'
 								placeholder='********'
+								value={this.confirmPassword}
+								onChange={e => {
+									this.handleChange(e);
+								}}
 							/>
+							{/** FeedbackMsg for Successful/Unsuccessful Password Match */}
+							<FormFeedback valid>Successful Password Match</FormFeedback>
+							<FormFeedback>Please check your password again!</FormFeedback>
 						</FormGroup>
 					</Col>
 					<FormGroup check>
@@ -70,7 +135,7 @@ class Signup extends Component {
 							Is Seeker
 						</Label>
 					</FormGroup>
-					<Button>Sign-Up</Button>
+					<Button onClick={this.clickHandler}>Sign-Up</Button>
 				</Form>
 			</Container>
 		);
