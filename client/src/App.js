46982d5205134382dcac5f601bfb464215dd036d
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-11-13 16:00:35 -0600

    Update client/src/App.js
    Billing Route

--- client/src/App.js
+++ client/src/App.js
@@ -5,15 +5,18 @@ import Auth from "./features/auth/component";
 import "./App.css";
 import Landing from "./layouts/Landing";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+import Billing from "./Billing/Billing";
 class App extends Component {
-	render() {
-		return (
-			<div className="App">
-				<Route path="/auth" component={Auth} />
-				<Route path="/" exact component={Landing} />
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="App">
+        <Route path="/auth" component={Auth} />
+        <Route path="/" exact component={Landing} />
+        <Route path="/billing" component={Billing} />
+      </div>
+    );
+  }
 }
 
 export default App;
