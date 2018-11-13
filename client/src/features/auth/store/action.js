278d657ca6c2dab16c79d935f6087078e96c6e59
sibhat <sibhat.temelso@gmail.com>
2018-11-13 10:09:02 -0800

    url change to main heroku backend master branch

--- client/src/features/auth/store/action.js
+++ client/src/features/auth/store/action.js
@@ -4,7 +4,7 @@ import { addErrorHandler } from "./error";
 import axios from "axios";
 let URL =
 	process.env.URL || "https://django-deploy-heroku-backend.herokuapp.com";
-URL = "https://jobmatcher-api-dev.herokuapp.com";
+// URL = "https://jobmatcher-api-dev.herokuapp.com";
 
 const signUpHandler = user => ({
 	type: action.SIGNUP__USER,
