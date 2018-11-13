4b46d5c569e6e700295171306f04a62e28851182
shobanavv <shobanavv@gmail.com>
2018-11-13 10:17:10 -0800

    HamburgerMenu setup for Navbar

---
+++ client/src/features/auth/component/HamburgerMenu/HamburgerMenu.js
@@ -0,0 +1,10 @@
+import React, { Component, Fragment } from 'react';
+//Fragments let you group a list of children without adding extra nodes to the DOM
+import { slide as Menu } from 'react-burger-menu';
+
+class HamburgerMenu extends Component {
+    handleSignOut = () => {
+      //call signout
+    }
+}
+export default HamburgerMenu;
