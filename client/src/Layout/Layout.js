c21a3f778e7bdfeb374bd2ba853fb40244e927e3
sibhat <sibhat.temelso@gmail.com>
2018-11-15 13:02:56 -0800

    path fixed

--- client/src/Layout/Layout.js
+++ client/src/Layout/Layout.js
@@ -1,34 +1,34 @@
 import React from "react";
 import { Container, Row } from "reactstrap";
 import { Route, Switch } from "react-router-dom";
-import Auth from "../features/auth/component/Auth";
+import Auth from "../features/auth/container/Auth";
 import ProtectedPages from "../features/auth/container/ProtectedPages";
 import Testing from "../features/testing/container";
 import NavBar from "../features/nav/component/NavBar";
-import Landing from "../layouts/Landing";
+import Landing from "../layouts/landingPage/Landing";
 import Billing from "../features/Billing/Billing";
 
 export default class Layout extends React.Component {
-  render() {
-    return (
-      <div>
-        <NavBar />
-        <Route path="/" exact component={Landing} />
-        <Container>
-          <Row>
-            <Switch>
-              <Route path="/auth" component={Auth} />
-              <Route path="/billing" component={Billing} />
+	render() {
+		return (
+			<div>
+				<NavBar />
+				<Route path="/" exact component={Landing} />
+				<Container>
+					<Row>
+						<Switch>
+							<Route path="/auth" component={Auth} />
+							<Route path="/billing" component={Billing} />
 
-              <Route
-                path="/testing"
-                exact
-                component={ProtectedPages(Testing)}
-              />
-            </Switch>
-          </Row>
-        </Container>
-      </div>
-    );
-  }
+							<Route
+								path="/testing"
+								exact
+								component={ProtectedPages(Testing)}
+							/>
+						</Switch>
+					</Row>
+				</Container>
+			</div>
+		);
+	}
 }
