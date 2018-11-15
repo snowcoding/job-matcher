6f14f482a157fb91200871e6a3341eb7e9b6a515
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-11-15 12:07:06 -0600

    Update Layout.js
    Add NavBar and Routes from App.js

--- client/src/Layout/Layout.js
+++ client/src/Layout/Layout.js
@@ -1,12 +1,27 @@
 import React from "react";
-import { Container, Row, Col } from "reactstrap";
+import { Container, Row } from "reactstrap";
+import { Route, Switch } from "react-router-dom";
+import Auth from "../features/auth/component/Auth";
+import ProtectedPages from "../features/auth/container/ProtectedPages";
+import Testing from "../features/testing/container";
+import NavBar from "../features/nav/component/NavBar";
+import Landing from "../layouts/Landing";
+import Billing from "../features/Billing/Billing";
 
-export default class Example extends React.Component {
+export default class Layout extends React.Component {
   render() {
     return (
       <Container>
         <Row>
-          <Col />
+          <NavBar />
+        </Row>
+        <Row>
+          <Switch>
+            <Route path="/auth" component={Auth} />
+            <Route path="/" exact component={Landing} />
+            <Route path="/billing" component={Billing} />
+            <Route path="/testing" exact component={ProtectedPages(Testing)} />
+          </Switch>
         </Row>
       </Container>
     );
