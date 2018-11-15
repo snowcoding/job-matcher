6c9d687a4151c09cf47056fba531473ac1c86b63
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-11-15 12:07:29 -0600

    Update App.js
    Moved NavBar and Routes to Layout.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,16 @@
 import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
-import Auth from "./features/auth/component/Auth";
-import ProtectedPages from "./features/auth/container/ProtectedPages";
-import Testing from "./features/testing/container";
-import NavBar from "./features/nav/component/NavBar.jsx"
 import "./App.css";
-import Landing from "./layouts/Landing";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Layout from "./Layout/Layout";
 
-import Billing from "./Billing/Billing";
 class App extends Component {
-
-	render() {
-		return (
-			<div className="App">
-				<NavBar />
-				<Switch>
-					<Route path="/auth" component={Auth} />
-					<Route path="/" exact component={Landing} />
-          			<Route path="/billing" component={Billing} />
-					<Route
-						path="/testing"
-						exact
-						component={ProtectedPages(Testing)}
-					/>
-				</Switch>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="App">
+        <Layout />
+      </div>
+    );
+  }
 }
 
 export default App;
