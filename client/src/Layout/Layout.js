378a1dbead62b7f111ff77e663a22f36fb48c8ee
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-11-15 11:33:44 -0600

    Create Layout.js

---
+++ client/src/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+
+export default class Example extends React.Component {
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col />
+        </Row>
+      </Container>
+    );
+  }
+}
