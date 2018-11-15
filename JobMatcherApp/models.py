219a2185686abe1267b05d00cf7fe774d211bee3
snowcoding <vimalshah77@gmail.com>
2018-11-15 10:34:39 +0100

    Add User model
    - Subclassing AbstractUser to enable Django admin features: auth, admin, permissions
    - `is_seeker` and `is_employer` help identify profile type to be implemented next

--- JobMatcherApp/models.py
+++ JobMatcherApp/models.py
@@ -1,6 +1,7 @@
 import uuid
 
 from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin, BaseUserManager, AbstractUser
 from django.db import models
 from django.utils.http import int_to_base36
 from django.utils.translation import gettext_lazy as _
@@ -25,3 +26,55 @@ class BaseModel(models.Model):
 
     class Meta:
         abstract = True
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Creates and saves a user with the given email, and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser, BaseModel):
+    username = None
+    email = models.EmailField(_('email address'), max_length=255, unique=True)
+    is_seeker = models.BooleanField(_('seeker'), default=False)
+    is_employer = models.BooleanField(_('employer'), default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+    def __str__(self):
+        return f'{self.id}: {self.email}'
+
+
