ce285b28017635378099bfbde4e57c04f23067ef
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-11-23 15:59:10 -0600

    Update views.py
    Add stripe configs and test charge endpoint

--- JobMatcherApp/views.py
+++ JobMatcherApp/views.py
@@ -7,6 +7,12 @@ from rest_framework.response import Response
 from JobMatcherApp.models import Employer, Seeker
 from . import serializers
 
+# Stripe configs
+import stripe
+from django.conf import settings
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
+
 
 @swagger_auto_schema(methods=['get'],
                      responses={200: openapi.Response('Seeker or Employer profile', serializers.ProfileSerializer)})
@@ -44,6 +50,19 @@ def signup_employer(request):
     serializer.is_valid(raise_exception=True)
     serializer.save()
     return Response(data=serializer.data)
+
+
+@api_view(http_method_names=['POST'])
+@permission_classes([])
+def charge(request):
+    status = stripe.Charge.create(
+        amount=500,
+        currency='usd',
+        description='A test charge from Django',
+        source=request.body
+    )
+
+    return Response(data={'status': status})
 
 
 class SeekerViewSet(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
