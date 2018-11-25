12f79fb5a981705b7242ea9026a35a0bd3f4f73e
snowcoding <vimalshah77@gmail.com>
2018-11-24 19:16:08 -0800

    Add missing layout and nav bar

--- client/src/Layout/Layout.js
+++ client/src/Layout/Layout.js
@@ -8,30 +8,33 @@ import NavBar from "../features/nav/component/NavBar";
 import Landing from "../layouts/landingPage/Landing";
 import Billing from "../features/Billing/Billing";
 import ProfileContainer from "../features/forms/containers";
-import  BrowseContainer from '../features/profile/container';
+import BrowseContainer from "../features/profile/container";
+import Job from "../features/job/component/Job";
 
 export default class Layout extends React.Component {
-	render() {
-		return (
-			<div>
-				<NavBar />
-				<Route path="/" exact component={Landing} />
-				<Container>
-					<Row>
-						<Switch>
-							<Route path="/auth" component={Auth} />
-							<Route path="/billing" component={Billing} />
-							<Route path="/rand" component={BrowseContainer} />
-							{/* <Route
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <Route path="/" exact component={Landing} />
+        <Container>
+          <Row>
+            <Switch>
+              <Route path="/auth" component={Auth} />
+              <Route path="/billing" component={Billing} />
+              <Route path="/rand" component={BrowseContainer} />
+              {/* <Route
 								path="/forms"
 								component={ProtectedPages(ProfileContainer)}
 							/> */}
-							<Route path="/profile" component={ProfileContainer} />
-							<Route path="/testing" exact component={Testing} />
-						</Switch>
-					</Row>
-				</Container>
-			</div>
-		);
-	}
+              <Route path="/profile" component={ProfileContainer} />
+              <Route path="/job" component={Job} />
+
+              <Route path="/testing" exact component={Testing} />
+            </Switch>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
--- client/src/features/nav/component/NavBar.jsx
+++ client/src/features/nav/component/NavBar.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { Link, NavLink } from 'react-router-dom'
+import React from "react";
+import { connect } from "react-redux";
+import { Link, NavLink } from "react-router-dom";
 import { logOut } from "../../auth/store/action";
 import {
   Navbar,
@@ -9,18 +9,16 @@ import {
   UncontrolledDropdown,
   DropdownToggle,
   DropdownMenu,
-  DropdownItem } from 'reactstrap';
-
+  DropdownItem
+} from "reactstrap";
 
 class NavBar extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-
-    };
+    this.state = {};
   }
 
-  handleSignOut = (e) => {
+  handleSignOut = e => {
     e.preventDefault();
     this.props.onLogOut();
   };
@@ -29,22 +27,37 @@ class NavBar extends React.Component {
     return (
       <div>
         <Navbar color="light" light expand="md">
-          <NavLink to="/" activeStyle={{
-                            fontWeight: "bold",
-                            textDecoration: 'none',
-                            color: "#4D4D4D",
-                          }}>Job Matcher</NavLink>
-          { this.props.authenticatoin_succeed &&
+          <NavLink
+            to="/"
+            activeStyle={{
+              fontWeight: "bold",
+              textDecoration: "none",
+              color: "#4D4D4D"
+            }}
+          >
+            Job Matcher
+          </NavLink>
+          {this.props.authenticatoin_succeed && (
             <Nav className="ml-auto" navbar>
               <NavItem>
-                <NavLink to="/components/" activeStyle={{
-                                            margin: "2px",
-                }}>Free Apps: </NavLink>
+                <NavLink
+                  to="/components/"
+                  activeStyle={{
+                    margin: "2px"
+                  }}
+                >
+                  Free Apps:{" "}
+                </NavLink>
               </NavItem>
               <NavItem>
-                <NavLink to="give a path" activeStyle={{
-                                        margin: "2px",
-                  }}>Balance credits: </NavLink>
+                <NavLink
+                  to="give a path"
+                  activeStyle={{
+                    margin: "2px"
+                  }}
+                >
+                  Balance credits:{" "}
+                </NavLink>
               </NavItem>
               <UncontrolledDropdown nav inNavbar>
                 <DropdownToggle nav caret>
@@ -52,62 +65,58 @@ class NavBar extends React.Component {
                 </DropdownToggle>
                 <DropdownMenu right>
                   <DropdownItem>
-                    <Link to ="/">
-                        Home
-                    </Link>
+                    <Link to="/">Home</Link>
                   </DropdownItem>
                   <DropdownItem>
-                    <Link to="/profile">
-                        My Profile
-                    </Link>
+                    <Link to="/profile">My Profile</Link>
                   </DropdownItem>
                   <DropdownItem>
-                    <Link to="/Matches">
-                        Matches
-                    </Link>
+                    <Link to="/Matches">Matches</Link>
                   </DropdownItem>
                   <DropdownItem>
-                    <Link to="/Messages">
-                        Messages
-                    </Link>
+                    <Link to="/Messages">Messages</Link>
                   </DropdownItem>
                   <DropdownItem>
-                    <Link to="/Billing">
-                        Billing
-                    </Link>
+                    <Link to="/Billing">Billing</Link>
+                  </DropdownItem>
+                  <DropdownItem>
+                    <a href="/Job">Job</a>
                   </DropdownItem>
                   <DropdownItem divider />
                   <DropdownItem>
-                    <Link to ="/" onClick={this.handleSignOut}>Sign Out</Link>
+                    <Link to="/" onClick={this.handleSignOut}>
+                      Sign Out
+                    </Link>
                   </DropdownItem>
                 </DropdownMenu>
               </UncontrolledDropdown>
             </Nav>
-          }
+          )}
         </Navbar>
       </div>
     );
   }
 }
 
-
 function mapStateToProps(state) {
-    return {
-        ...state.user
-    };
+  return {
+    ...state.user
+  };
 }
 
 const mapDispatchToProps = dispatch => ({
-  onLogOut: (user) => {
+  onLogOut: user => {
     dispatch(logOut());
-  },
+  }
 });
 
-
-const ConnectedNavBar = connect(mapStateToProps, mapDispatchToProps)(NavBar);
+const ConnectedNavBar = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(NavBar);
 
 export default class Wrapper extends React.Component {
-    render(){
-       return (<ConnectedNavBar />);
-    }
+  render() {
+    return <ConnectedNavBar />;
+  }
 }
