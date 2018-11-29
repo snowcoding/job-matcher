24534eb490d5003fbaa2a362e13459fddd3f1bd1
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-11-29 17:11:12 -0600

    Update index.js

--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -1,9 +1,10 @@
 import userReduceer from "../../features/auth/store/reducer";
 import userProfileReducer from "../../features/profile/store/reducer";
+import billingReducer from "../../features/billing/store/reducer";
 
 import { combineReducers } from "redux";
 export default combineReducers({
-	user: userReduceer,
-	randomUser: userProfileReducer
-
+  user: userReduceer,
+  randomUser: userProfileReducer,
+  billing: billingReducer
 });
