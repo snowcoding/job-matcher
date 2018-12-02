473e3b9b71ed9e8fc0d64134375819d005b414b9
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-12-02 16:28:00 -0600

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/css/blk-design-system.css";
+// import "./assets/css/nucleo-icons.css";
 import Layout from "./Layout/Layout";
 
 class App extends Component {
