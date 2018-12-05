4977d815255b5b143161c124561781880f7df7c0
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-12-04 18:00:36 -0600

    Revert "Update App.js"
    This reverts commit 473e3b9b71ed9e8fc0d64134375819d005b414b9.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./assets/css/blk-design-system.css";
-// import "./assets/css/nucleo-icons.css";
 import Layout from "./Layout/Layout";
 
 class App extends Component {
