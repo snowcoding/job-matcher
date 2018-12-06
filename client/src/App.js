45992d6573d5d751fd5a52b602d412dd840768e5
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-12-06 00:12:33 -0600

    Revert "Merge branch 'blk-sidebar' into develop"
    This reverts commit 3ca5ca7f2f3259ae0758f94f407677dde862ef52, reversing
    changes made to 5309130964f751ca679f79202a6b29d58deedddc.

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,14 @@
 import React, { Component } from "react";
-import { Router, Route, Switch, Redirect } from "react-router-dom";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Layout from "./Layout/Layout";
 import { ToastContainer, Slide } from "react-toastify";
-// import "./App.css";
-// import "bootstrap/dist/css/bootstrap.min.css";
-import "assets/css/black-dashboard-react.css";
-import "assets/css/nucleo-icons.css";
-// import Layout from "./Layout/Layout";
-import AdminLayout from "layouts/Admin/Admin.jsx";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <Layout />  */}
-        <Switch>
-          <Route path="/" render={props => <AdminLayout {...props} />} />
-        </Switch>
+        <Layout />
         <ToastContainer transition={Slide} />
       </div>
     );
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-import { createBrowserHistory } from "history";
 
 // redux
 import storage from "redux-persist/lib/storage"; // defaults to localStorage for web and AsyncStorage for react-native
@@ -32,12 +31,10 @@ const store = createStore(
 // let store = createStore(persistedReducer)
 let persistor = persistStore(store);
 
-const hist = createBrowserHistory();
-
 ReactDOM.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <Router history={hist}>
+      <Router>
         <App />
       </Router>
       ,
