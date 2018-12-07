b218b4ee2f194c403bd520f5e4829e48268cf6d4
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-12-06 18:11:17 -0600

    Update App.js
    resolve merge conflict

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from "react";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Layout from "./Layout/Layout";
+import { Route, Switch, Redirect } from "react-router-dom";
 import { ToastContainer, Slide } from "react-toastify";
+// import "./App.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
+import "assets/css/black-dashboard-react.css";
+import "assets/css/nucleo-icons.css";
+// import Layout from "./Layout/Layout";
+import AdminLayout from "layouts/Admin/Admin.jsx";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Layout />
+        {/* <Layout />  */}
+        <Switch>
+          <Route path="/" render={props => <AdminLayout {...props} />} />
+        </Switch>
         <ToastContainer transition={Slide} />
       </div>
     );
